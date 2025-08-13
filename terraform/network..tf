c83f9cb7fa4b9ac7a55d5fd598eb31829f248424
Markin Aleksey <alexm97@mail.ru>
2025-08-13 19:29:57 +0300

    add nlb

--- terraform/network..tf
+++ terraform/network..tf
@@ -9,3 +9,59 @@ resource "yandex_vpc_subnet" "subnets" {
   network_id     = "${yandex_vpc_network.Diplom.id}"
   v4_cidr_blocks = [ "${var.cidrs[count.index]}" ]
 }
+
+resource "yandex_lb_target_group" "balancer-group" {
+  name       = "balancer-group"
+  depends_on = [yandex_compute_instance.master]
+  dynamic "target" {
+    for_each = yandex_compute_instance.worker
+    content {
+      subnet_id = target.value.network_interface.0.subnet_id
+      address   = target.value.network_interface.0.ip_address
+    }
+  }
+}
+
+resource "yandex_lb_network_load_balancer" "monapp" {
+  name = "monapp"
+  listener {
+    name        = "monapp-listener"
+    port        = 80
+    target_port = 80
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
+  attached_target_group {
+    target_group_id = yandex_lb_target_group.balancer-group.id
+    healthcheck {
+      name = "healthcheck"
+      tcp_options {
+        port = 80
+      }
+    }
+  }
+  depends_on = [yandex_lb_target_group.balancer-group]
+}
+
+resource "yandex_lb_network_load_balancer" "atlantis" {
+  name = "atlantis"
+  listener {
+    name        = "atlantis-listener"
+    port        = 80
+    target_port = 30080
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
+  attached_target_group {
+    target_group_id = yandex_lb_target_group.balancer-group.id
+    healthcheck {
+      name = "healthcheck"
+      tcp_options {
+        port = 30080
+      }
+    }
+  }
+  depends_on = [yandex_lb_network_load_balancer.monapp]
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -11,3 +11,13 @@ output "cluster-k8s-workers" {
    "name = ${i.name} | external-ip=${i.network_interface.0.ip_address} | internal-ip=${i.network_interface.0.nat_ip_address}"
   ]
 }
+
+output "Grafana_Network_Load_Balancer_Address" {
+  value = yandex_lb_network_load_balancer.monapp.listener.*.external_address_spec[0].*.address
+  description = "Адрес сетевого балансировщика для Grafana и Тестового приложения"
+}
+
+output "Web_App_Network_Load_Balancer_Address" {
+  value = yandex_lb_network_load_balancer.atlantis.listener.*.external_address_spec[0].*.address
+  description = "Адрес сетевого балансировщика Atlantis"
+}
